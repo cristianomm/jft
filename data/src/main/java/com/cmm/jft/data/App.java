4cb0e88c96321ddf0f3797da56db5693e8e73e15
Cristiano M Martins <cristranus@gmail.com>
2015-09-03 01:24:01 -0300

    documentacao.

--- data/src/main/java/com/cmm/jft/data/App.java
+++ data/src/main/java/com/cmm/jft/data/App.java
@@ -1,13 +1,26 @@
 package com.cmm.jft.data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	/*
+	 * this is a multiline comment block
+	 */
     public static void main( String[] args )
     {
+    	int intvar = 0;
+    	String strVar = "text";
         System.out.println( "Hello World!" );
     }
+    
+    
+    
 }
