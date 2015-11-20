ec473710cb73f59de0e703c146a2eb4960e7c187
Cristiano M Martins <cristranus@gmail.com>
2015-11-20 16:25:40 -0200

    services.

--- services/src/main/java/com/cmm/jft/services/marketdata/MarketDataService.java
+++ services/src/main/java/com/cmm/jft/services/marketdata/MarketDataService.java
@@ -3,26 +3,17 @@
  */
 package com.cmm.jft.services.marketdata;
 
+import java.time.LocalTime;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import com.cmm.jft.connector.marketdata.MarketDataConnector;
-import com.cmm.jft.services.security.SecurityService;
-import com.cmm.jft.vo.NewsVO;
-
 import quickfix.DataDictionary;
-import quickfix.Dictionary;
-import quickfix.Field;
 import quickfix.FieldException;
 import quickfix.FieldNotFound;
-import quickfix.FieldType;
-import quickfix.FixVersions;
 import quickfix.Group;
-import quickfix.InvalidMessage;
 import quickfix.Message;
 import quickfix.field.MDEntryDate;
 import quickfix.field.MDEntryPositionNo;
@@ -49,8 +40,11 @@ import quickfix.fix50.MarketDataSnapshotFullRefresh;
 import quickfix.fix50.News;
 import quickfix.fix50.SecurityList;
 import quickfix.fix50.SecurityStatus;
-import quickfix.fix50.component.Instrument;
-import quickfix.fix50.component.SecListGrp;
+
+import com.cmm.jft.connector.marketdata.MarketDataConnector;
+import com.cmm.jft.messaging.LogFile;
+import com.cmm.jft.services.security.SecurityService;
+import com.cmm.jft.vo.NewsVO;
 
 /**
  * <p><code>MarketDataService.java</code></p>
@@ -69,66 +63,27 @@ public class MarketDataService {
 
 	private LinkedList<NewsVO> newsFeed;
 	private ConcurrentLinkedQueue<Snapshot> snapshots;
-
-
 	private static MarketDataService instance;
-	
-	private char delimiter = 0;
-	public static char getDelimiter( String line ) {
-		if( line.length() < 10 )
-			return 0;
-		return line.charAt(9);
-	}
-	
-	private Message parseLine( String line, DataDictionary dataDictionary ) {
-		/*if( skipLine(line, type) ) {
-			return null;
-		}*/
-		
-		try {
-			String[] split = line.split("8=",2);
-			if( split.length != 2 )
-				return null;
-			line = "8=" + split[1];
-			
-			if( delimiter == 0 )
-				delimiter = getDelimiter(line);
-			String regexp = "[" + delimiter + "]";
-			if( delimiter == '^' ) regexp = "[\\^].";
-			if( delimiter != '\001' && delimiter != 0 )
-				line = line.replaceAll(regexp, "\001");
-			int lastChar = line.lastIndexOf('\001');
-			if( lastChar != line.length() )
-				line = line.substring(0, lastChar+1);
-			if( line == null )
-				return null;
-			if( dataDictionary == null )
-				return new Message( line, false );
-			else
-				return new Message( line, dataDictionary, false );
-		} catch( quickfix.InvalidMessage e1 ) {
-			return null;
-		} catch( ArrayIndexOutOfBoundsException e2 ) {
-			return null;
-		}
-	}
 	
 	public static void main(String[] args){
 		try{
-			String msg = "8=FIX.4.49=43335=X34=249=TradingEngineDerivativesA52=20071106-19:12:48.06356=FIXGatewayDerivatives_MD10016=BMFBR9200444_175=20071106268=3279=1269=c278=155=COTZ0748=BMFBR920044422=8272=20071106273=19:12:19336=TradingSessionID326=101279=1269=b278=255=COTZ0748=BMFBR920044422=8272=20071106273=19:12:23336=TradingSessionID625=S279=0269=555=COTZ0748=BMFBR920044422=8270=0272=20071106273=19:12:38336=TradingSessionID10=161";
+						
+			String msg = "8=FIX.4.49=43335=X34=249=TradingEngineDerivativesA52=20071106-19:12:48.06356=FIXGatewayDerivatives_MD10016=BMFBR9200444_175=20071106268=3279=1269=c278=155=COTZ0748=BMFBR920044422=8272=20071106273=19:12:19336=TradingSessionID326=101279=1269=b278=255=COTZ0748=BMFBR920044422=8272=20071106273=19:12:23336=TradingSessionID625=S279=0269=555=COTZ0748=BMFBR920044422=8270=0272=20071106273=19:12:38336=TradingSessionID10=161";
 			//msg = "35=y1128=934=252=20140610132615107393=111893=N146=555=FESA448=380342622=8207=BVMF1351=31180=MBO511180=MBP1511141=11022=STD264=101180=TOB2511141=11022=STD264=1454=2455=BMFBR3803426456=8455=BRFESAACNPR5456=4870=2871=34872=1871=24872=1980=M1234=11093=21231=100969=0.015151=29749=1009748=588800015=BRL120=BRL460=5167=PS762=10046937=FESA107=FERBASA     PN      N17595=58880000541=99991231200=999912231=1667=201404461=EPNEFR470=BR225=2014042963=D164=999912316938=99991231-22:59:59.0001300=8037011=N137010=461151=0755=FBMC348=380347522=8207=BVMF1351=31180=MBO511180=MBP1511141=11022=STD264=101180=TOB2511141=11022=STD264=1454=2455=BMFBR3803475456=8455=BRFBMCACNOR3456=4870=2871=34872=1871=24872=1980=M1234=11093=21231=100969=0.015151=29749=1009748=2651615=BRL120=BRL460=5167=CS762=10036937=FBMC107=FIBAM       ON7595=265160541=99991231200=999912231=1667=201312461=ESVUFR470=BR225=2013122663=D164=999912316938=99991231-22:59:59.0001300=8037010=1381151=0155=FESA348=380353322=8207=BVMF1351=31180=MBO511180=MBP1511141=11022=STD264=101180=TOB2511141=11022=STD264=1454=2455=BMFBR3803533456=8455=BRFESAACNOR8456=4870=2871=34872=1871=24872=1980=M1234=11093=21231=100969=0.015151=29749=1009748=294400015=BRL120=BRL460=5167=CS762=10036937=FESA107=FERBASA     ON      N17595=29440000541=99991231200=999912231=1667=201404461=ESVUFR470=BR225=2014042963=D164=999912316938=99991231-22:59:59.0001300=8037011=N137010=1501151=0155=FBMC448=380357422=8207=BVMF1351=31180=MBO511180=MBP1511141=11022=STD264=101180=TOB2511141=11022=STD264=1454=2455=BMFBR3803574456=8455=BRFBMCACNPR0456=4870=2871=34872=1871=24872=1980=M1234=11093=21231=100969=0.015151=29749=1009748=4613515=BRL120=BRL460=5167=PS762=10046937=FBMC107=FIBAM       PN7595=461354541=99991231200=999912231=1667=201312461=EPNEFR470=BR225=2013122663=D164=999912316938=99991231-22:59:59.0001300=8037010=1501151=0155=GGBR348=380388922=8207=BVMF1351=31180=MBO511180=MBP1511141=11022=STD264=101180=TOB2511141=11022=STD264=1454=2455=BMFBR3803889456=8455=BRGGBRACNOR1456=4870=2871=34872=1871=24872=1980=M1234=11093=21231=100969=0.015151=29749=1009748=5736274815=BRL120=BRL460=5167=CS762=10036937=GGBR107=GERDAU      ON      N17595=573627483541=99991231200=999912231=1667=201405461=ESVUFR470=BR225=2014052263=D164=999912316938=99991231-22:59:59.0001300=8037011=N137010=1541151=07";
 			
-			Message m = new MarketDataService().parseLine(msg, new DataDictionary("FIX44.xml"));
+			LogFile lf = new LogFile("C:/Users/COC-DELL-02/Downloads/LOGs/Log Atualizacoes Market Data Msg X.log", new DataDictionary("FIX44.xml"));
 			
-			//sl.fromString(msg, new DataDictionary("FIX44.xml"), false);
-			List<Group> gs = m.getGroups(268);
-			for (Group group : gs) {
-				System.out.println(group.getString(Symbol.FIELD));
-			}
+			lf.parseNewMessages();
 			
-		}/*catch(InvalidMessage e){
+			Message m = (Message) lf.getMessages().get(4);// parseTextMessage(msg);
+			
+			System.out.println(lf.getGroups(m).size());
+						
+		}
+		/*catch(InvalidMessage e){
 			e.printStackTrace();
-		}*/ catch (Exception e) {
+		}*/ 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -319,16 +274,14 @@ public class MarketDataService {
 
 	private void consumeSecurityList(SecurityList securityList){
 		try {
-
 			int mdMsgSeq = securityList.getHeader().getInt(34);
-
+			
 			if(mdMsgSeq == 1){
 				SecurityService.getInstance().restartSecurityList();
 				//verifica se recebeu todos instrumentos
 				int totNumRelSym = securityList.getTotNoRelatedSym().getValue();
 				boolean lstFrag = securityList.getLastFragment().getValue();
 				if(totNumRelSym > SecurityService.getInstance().getSecurityList().size() && !lstFrag) {
-					
 					
 					//quantidade de simbolos nesta msg
 					int related = securityList.getNoRelatedSym().getValue();
@@ -344,7 +297,7 @@ public class MarketDataService {
 			}
 
 		} catch(FieldNotFound e) {
-
+			
 		}
 
 	}
@@ -352,10 +305,10 @@ public class MarketDataService {
 	private void consumeSecurityStatus(SecurityStatus securityStatus){
 
 		//pra cada security que recebeu o status
-
+		
 
 		//ajusta o status da security em securityService
-
+		
 
 	}
 
