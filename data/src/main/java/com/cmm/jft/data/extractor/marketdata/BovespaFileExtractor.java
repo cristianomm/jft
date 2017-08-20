63dc82d856f81fa2fffffeae97780f3f02e4ac5c
cristiano <cristiano.mrmartins@gmail.com>
2017-08-20 14:08:18 -0300

    code organization.

--- data/src/main/java/com/cmm/jft/data/extractor/marketdata/BovespaFileExtractor.java
+++ data/src/main/java/com/cmm/jft/data/extractor/marketdata/BovespaFileExtractor.java
@@ -25,46 +25,46 @@ import com.cmm.logging.Logging;
  */
 public abstract class BovespaFileExtractor implements Extractor {
 
-	protected String fileName;
-	protected Scanner scanner;
-	
-	protected Pattern pTime = Pattern.compile("[\\d\\d|:]+[.|:|,][\\d]{3}");
-	protected Matcher matcher = pTime.matcher("");
-	
-	public static void main(String[] args) {
+    protected String fileName;
+    protected Scanner scanner;
 
-		Properties p = new Properties();
-		p.put("filename", "D:/Development/Study/Math/COTAHIST_A2014.TXT");
+    protected Pattern pTime = Pattern.compile("[\\d\\d|:]+[.|:|,][\\d]{3}");
+    protected Matcher matcher = pTime.matcher("");
 
-		BovespaHistoricalFileExtractor be = new BovespaHistoricalFileExtractor();
-		be.config(p);
+    public static void main(String[] args) {
 
-		TimeCounter tc = new TimeCounter();
-		tc.start();
-		List l = be.extract();
-		tc.stop();
+	Properties p = new Properties();
+	p.put("filename", "D:/Development/Study/Math/COTAHIST_A2014.TXT");
 
-		System.out.println(l.size() + " Lines in: " + tc.getElapsedInSeconds());
+	BovespaHistoricalFileExtractor be = new BovespaHistoricalFileExtractor();
+	be.config(p);
 
-	} 
+	TimeCounter tc = new TimeCounter();
+	tc.start();
+	List l = be.extract();
+	tc.stop();
+
+	System.out.println(l.size() + " Lines in: " + tc.getElapsedInSeconds());
+
+    } 
 
 
-	/* (non-Javadoc)
-	 * @see com.cmm.jft.data.extractor.Extractor#config(java.util.Properties)
-	 */
-	@Override
-	public boolean config(Properties properties) {
-		boolean ret = false;
-		if(properties!= null && properties.containsKey("filename")){
-			this.fileName = properties.getProperty("filename");
-			try {
-				this.scanner = new Scanner(new File(fileName));
-				ret = true;
-			} catch (FileNotFoundException e) {
-				ret = false;
-				Logging.getInstance().log(getClass(), e, Level.ERROR);
-			}
-		}
-		return ret;
+    /* (non-Javadoc)
+     * @see com.cmm.jft.data.extractor.Extractor#config(java.util.Properties)
+     */
+    @Override
+    public boolean config(Properties properties) {
+	boolean ret = false;
+	if(properties!= null && properties.containsKey("filename")){
+	    this.fileName = properties.getProperty("filename");
+	    try {
+		this.scanner = new Scanner(new File(fileName));
+		ret = true;
+	    } catch (FileNotFoundException e) {
+		ret = false;
+		Logging.getInstance().log(getClass(), e, Level.ERROR);
+	    }
 	}
+	return ret;
+    }
 }
